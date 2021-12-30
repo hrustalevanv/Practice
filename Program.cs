ee14b76b66b43b8a956fa5ba090deaca186ad3fd
hrustalevanv <hrustalevanv2021@gmail.com>
2021-12-30 13:20:34 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿//Написать программу, которая из имеющегося массива
+// целых чисел формирует массив из четных чисел.
+
+int [] numbers = {2, 3, 4, 5, 7, 1, 8};//объявляем массив
+int k = 0;
+int j = 0;
+
+//считаем количество четных чисел
+for (int i = 0; i < numbers.Length; i++)
+{
+  if (numbers[i] % 2 == 0)
+  {
+    k++;
+  }
+}
+
+int [] array = new int [k]; //создаем массив
+
+//наполняем массив
+for (int i = 0; i < numbers.Length; i++)
+{
+  if (numbers[i] % 2 == 0)
+  {
+    array [j] = numbers [i];
+    j++;
+  } 
+}
+
+//выводим в консоль
+for (int i = 0; i < array.Length; i++)
+{
+  Console.WriteLine (array [i]);
+}
+
