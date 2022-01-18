edd144683a3c15fbafd41a2d5e979e664a71fc17
hrustalevanv <hrustalevanv2021@gmail.com>
2022-01-18 16:36:02 +0300

    Modification01

--- Program.cs
+++ Program.cs
@@ -1,34 +1,74 @@
 ﻿//Написать программу, которая из имеющегося массива
 // целых чисел формирует массив из четных чисел.
 
-int [] numbers = {2, 3, 4, 5, 7, 1, 8};//объявляем массив
-int k = 0;
-int j = 0;
 
-//считаем количество четных чисел
-for (int i = 0; i < numbers.Length; i++)
+//Однако блок-схемы у Вас вообще не соответствуют указанной задаче. 
+//Код присутствует и работает, однако необходимо было разбивать его на методы,
+ //так же не использовать Console.Write внутри методов, кроме метода печати.
+
+//создаем массив
+int [] numbers = new int [7]; //исходный массив
+int k = 0; //количество четных чисел в исходном массиве
+//int j = 0;
+
+//метод наполнения массива
+int[] napolnim(int [] arnapolnim)
 {
-  if (numbers[i] % 2 == 0)
+  for (int i = 0; i < arnapolnim.Length; i++)
   {
-    k++;
+     arnapolnim[i] = new Random().Next(1, 10); 
   }
+    return arnapolnim;
 }
 
-int [] array = new int [k]; //создаем массив
-
-//наполняем массив
-for (int i = 0; i < numbers.Length; i++)
+//метод вывода на печать
+void pechat(int [] arpechat)
 {
-  if (numbers[i] % 2 == 0)
+  for (int i = 0; i < arpechat.Length; i++)
   {
-    array [j] = numbers [i];
-    j++;
+    Console.WriteLine (arpechat[i]);
   } 
 }
 
-//выводим в консоль
-for (int i = 0; i < array.Length; i++)
-{
-  Console.WriteLine (array [i]);
+//считаем количество четных чисел
+int chetnix(int [] archetnix)
+{ 
+  int ark = 0;
+  for (int i = 0; i < archetnix.Length; i++)
+  {
+    if (archetnix[i] % 2 == 0)
+      {
+         ark++;
+      }
+  }
+  return ark;
 }
+
+//метод возвращает массив четных чисел
+int [] napolnimchetn(int [] arnapolnimchetn)
+{
+  int [] archetn = new int [k];
+  int j = 0;
+   for (int i = 0; i < arnapolnimchetn.Length; i++)
+  {
+     if (arnapolnimchetn[i] % 2 == 0)
+     {
+       archetn[j] = arnapolnimchetn[i];
+       j++;
+     } 
+  }
+  return archetn;
+}
+
+numbers = napolnim(numbers);//наполняем исходный массив
+
+Console.WriteLine($"Выводим массив исходных чисел ");
+
+pechat(numbers); //выводим на печать исходный массив
+
+k = chetnix(numbers);//считаем количество четных чисел в массиве и кладет в переменную "к"
+
+Console.WriteLine($"Выводим массив четных чисел ");
+
+pechat(napolnimchetn(numbers));
 
